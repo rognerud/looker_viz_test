ed513d4aea6d758d5f93d35ca3b5bea7cba0c085
girog161 <gisle.rognerud@tv2.no>
2022-10-15 23:42:18 +0200

    woah

--- flex_multi_kpi28.js
+++ flex_multi_kpi28.js
@@ -40,7 +40,7 @@ looker.plugins.visualizations.add({
         section: "KPI1",
         default: "#000000"
       },
-      kpi_1_positive_icon: {
+      kpi_1_icon: {
         type: "string",
         label: "display icon for kpi 1",
         display: "string",
@@ -90,7 +90,7 @@ looker.plugins.visualizations.add({
         values: [
           {"Color comparison value": "comparison-value"},
           {"Color comparison icon": "comparison-icon"},
-          {"Color comparison": "comparison-both"},
+          {"Color comparison": "comparison"},
           {"Color main value": "main-value"},
           {"Color main icon": "main-icon"},
           {"Color main": "main"},
@@ -105,6 +105,20 @@ looker.plugins.visualizations.add({
         section: "Main",
         default: false,
       },
+      main_element_negative_color: {
+        type: "string",
+        label: "color for negative values",
+        display: "color",
+        section: "Main",
+        default: "#ff0000"
+      },
+      main_element_positive_color: {
+        type: "string",
+        label: "color for positive values",
+        display: "color",
+        section: "Main",
+        default: "#00ff00"
+      }
     },
   
     create: function(element, config) {
@@ -112,114 +126,57 @@ looker.plugins.visualizations.add({
       // Insert a <style> tag with some styles we'll use later.
       element.innerHTML = `
       <style>
-        .card {
-          margin: 0px 0px 0rem;
-          padding: 0px;
-          font-family: "Google Sans", "Noto Sans", "Noto Sans JP", "Noto Sans CJK KR", "Noto Sans Arabic UI", "Noto Sans Devanagari UI", "Noto Sans Hebrew", "Noto Sans Thai UI", Helvetica, Arial, sans-serif;
-          box-sizing: border-box;
-          display: block;
-          font-size: 1.17em;
-          margin-block-start: 1em;
-          margin-block-end: 1em;
-          margin-inline-start: 0px;
-          margin-inline-end: 0px;
-        }
-
-        .card-block {
-          -webkit-box-align: center;
-          align-items: center;
-          flex-direction: column;
-          height: 100%;
-          -webkit-box-pack: center;
-          justify-content: center;
-          overflow: hidden;
-          position: relative;
-          width: 100%;
-          max-height: 100%;
-        }
-
-        .card-main {
-          width: 100%;
-          display: flex;
-          justify-content: stretch;
-          flex-direction: column;
-          padding: 0px 12px;
-        }
-
-        .card-comparison {
-          max-width: 100%;
-          flex: 1 1 0%;
-          display: flex;
-          -webkit-box-align: stretch;
-          align-items: stretch;
-          -webkit-box-pack: end;
-          justify-content: flex-end;
-          overflow-y: auto;
-          flex-direction: column;
-          margin-bottom: 0.75rem;
-        }
-
-        .kpi-value {
-          font-size: 2.25rem;
-          letter-spacing: -1px;
-          text-align: center;
-          display: inline-block;
-          overflow: hidden;
-          text-overflow: ellipsis;
-          white-space: nowrap;
+      .card {
+        margin: auto;
+        width: 100%;
+        height: 100%;
+        padding: 0.5rem;
+        text-align: center;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        white-space: nowrap;
+        display: inline-block;
+        font-family: "Google Sans", "Noto Sans", "Noto Sans JP", "Noto Sans CJK KR", "Noto Sans Arabic UI", "Noto Sans Devanagari UI", "Noto Sans Hebrew", "Noto Sans Thai UI", "Helvetica", "Arial", "sans-serif";
+      }
+      
+      .card-icon {
+        padding-top: 2rem;
+      }
+      
+      .card-value {
+          padding: 0.75rem;
+          font-size: 3rem;
           font-weight: 400;
-          line-height: 1.5em;
-        }
-        .kpi-title {
-          font-size: 0.875rem;
-          overflow: hidden;
-          text-overflow: ellipsis;
-          white-space: nowrap;
-          line-height: 1.25rem;
-          text-align: centre
-        }
-        .kpi-comparison {
-          font-family: inherit;
-          margin: 0px;
-          padding: 0px 0.75rem;
-          min-width: 1px;
-          width: 100%;
-          display: flex;
-          height: 36px;
-          position: absolute;
-          bottom: 0px;
-          left: 0px;
-          flex: 1 1 0%;
-          -webkit-box-align: center;
-          align-items: center;
-          -webkit-box-pack: center;
-          justify-content: center;
-        }
+      }
+      
+      .card-title {
+          padding: 0.25rem;
+          font-size: 1.5rem;  
+      }
+      
+      .card-comparison {
+        padding: 0.25rem;
+        margin: 0 auto;
+      }
 
       </style>
-      <div class="card">
-      <div class="card-block">
-        <div class="card-main">
-          <div class="kpi-icon id="kpi-1-icon></div>
-          <div class="kpi-value" id="kpi-1-value">100 000</div>
-          <div class="kpi-title" id="kpi-1-title">users</div>
+
+      <div class="content-wrapper">
+      <div class="card" id="kpi-1">
+        <div class="card-icon">
+          <box-icon id="kpi-1-icon" type="solid" size="lg" color="#eb4034" name="rocket"></box-icon>
         </div>
+        <div class="card-value" id="kpi-1-value">50%</div>
+        <div class="card-title" id="kpi-1-title">title</div>
         <div class="card-comparison">
-          <div style="width: 100%; height: 36px;">
-            <div class="kpi-comparison">
-              <span class="kpi-comparison-value" id="kpi-1-1-value">5%</span>
-              <span class="kpi-comparison-title" id="kpi-1-1-title">ly</span>
-              </div>
-          </div>
+          <span class="card-comparison-value" id="kpi-1-1-value">15 pp</span>
+          <span class="card-comparison-title" id="kpi-1-1-title">mot y-1</span>    
+          <span class="card-comparison-value" id="kpi-1-2-value">10 pp</span>
+          <span class="card-comparison-title" id="kpi-1-2-title">mot m-1</span>   
         </div>
-      </div>
       </div>
 
       `;
-        // Create a container element to let us center the text.
-      var container = element.appendChild(document.createElement("div"));
-      container.id = "flex-multi-kpi";
-
     }, 
 
     updateAsync: function(data, element, config, queryResponse, details, done) {
@@ -235,55 +192,100 @@ looker.plugins.visualizations.add({
 
       var kpi_1_column = all_columns.at(config.kpi_1_column);
 
-      if (kpi_1_comparison_column_1 != null) {
-        var kpi_1_comparison_column_1 = all_columns.at(config.kpi_1_comparison_column_1);
-        if (config.kpi_1_comparison_title_1 == null) {
-          var kpi_1_comparison_title_1 = config.kpi_1_comparison_title_1;
-        } else {
-          var kpi_1_comparison_title_1 = all_columns.at(config.kpi_1_comparison_column_1);
+      document.getElementById("kpi-1-icon").setAttribute("name", config.kpi_1_icon);
+      document.getElementById("kpi-1-icon").setAttribute("color", config.kpi_1_icon_color);
+      document.getElementById("kpi-1-value").innerHTML = firstRow[kpi_1_column.name].rendered;
+      
+      if (config.kpi_1_title != "") {
+        document.getElementById("kpi-1-title").innerHTML = config.kpi_1_title;
+      } else {
+        document.getElementById("kpi-1-title").innerHTML = kpi_1_column.label_short;
+      }
+      document.getElementById("kpi-1-title").style.color = config.kpi_1_title_color;
+      document.getElementById("kpi-1-value").style.color = config.kpi_1_value_color;
+
+
+      if (config.kpi_1_comparisons_visible) {
+        if (kpi_1_comparison_column_1 != null) {
+          var kpi_1_comparison_column_1 = all_columns.at(config.kpi_1_comparison_column_1);
+          document.getElementById("kpi-1-1-value").innerHTML = firstRow[kpi_1_comparison_column_1.name].rendered;
+          document.getElementById("kpi-1-1-title").innerHTML = config.kpi_1_comparison_title_1;
         }
-        
-      }
-
-      if (kpi_1_comparison_column_2 != null) {
-        var kpi_1_comparison_column_2 = all_columns.at(config.kpi_1_comparison_column_2);
-      }
-
-      if (config.kpi_1_icon != null) {
-        var kpi_1_icon = config.kpi_1_icon;
-      }
-
-      var kpi1_value = firstRow[kpi1.name].value;
-      
-      if (config.kpi_1_title != null) {
-        var kpi_1_title = config.kpi_1_title;
+        if (kpi_1_comparison_column_2 != null) {
+          var kpi_1_comparison_column_2 = all_columns.at(config.kpi_1_comparison_column_2);
+          document.getElementById("kpi-1-2-value").innerHTML = firstRow[kpi_1_comparison_column_2.name].rendered;
+          document.getElementById("kpi-1-2-title").innerHTML = config.kpi_1_comparison_title_2;
+        }
       } else {
-        var kpi_1_title = kpi_1_column.label_short;
+        document.getElementById("kpi-1-1-value").innerHTML = "";
+        document.getElementById("kpi-1-1-title").innerHTML = "";
+        document.getElementById("kpi-1-2-value").innerHTML = "";
+        document.getElementById("kpi-1-2-title").innerHTML = "";
       }
-      if (config.kpi_1_comparison_1_title != null) {
-        var kpi_1_comparison_1_title = config.kpi_1_comparison_1_title;
-      } else {
-        var kpi_1_comparison_1_title = kpi_1_column.label_short;
+
+      function adjustSizeKPI(size, kpi) {
+        if (size == "small") {
+          document.getElementById(kpi+"-value").style.fontSize = "2rem";
+        } else if (size == "medium") {
+          document.getElementById(kpi+"-value").style.fontSize = "3rem";
+        }
+        else if (size == "large") {
+          document.getElementById(kpi+"-value").style.fontSize = "4rem";
+        }
       }
-      var kpi1_comparison_title = config.kpi_1_comparison_title;
+      adjustSizeKPI(config.kpi_1_size, "kpi-1");
 
-      console.log(queryResponse);
+      var kpi1_comparison_value_1 = firstRow[kpi1_comparison_column_1.name].value;
+      var kpi1_comparison_value_2 = firstRow[kpi1_comparison_column_2.name].value;
 
-      function addKPI(kpi, title, id) {
-          var id_title = id + "-title";
-          var id_value_and_icon = id + "-value-and-icon";
-          document.getElementById(id_value_and_icon).innerHTML = kpi;
-          document.getElementById(id_title).innerHTML = title;
+      function colorizeKPImain(color, option) {
+        if (option == "comparison-value") {
+          document.getElementById("kpi-1-1-value").style.color = color;
+        } else if (option == "comparison-icon") {
+          document.getElementById("kpi-1-1-title").style.color = color;
+        } else if (option == "comparison") {
+          document.getElementById("kpi-1-1-value").style.color = color;
+          document.getElementById("kpi-1-1-title").style.color = color;
+        } else if (option == "main-value") {
+          document.getElementById("kpi-1-value").style.color = color;
+        } else if (option == "main-icon") {
+          document.getElementById("kpi-1-icon").style.color = color;
+        } else if (option == "main") {
+          document.getElementById("kpi-1-value").style.color = color;
+          document.getElementById("kpi-1-icon").style.color = color;
+        }
+      }
+
+      function colorizeKPIsecondary(color, option) {
+        if (option == "comparison-value") {
+          document.getElementById("kpi-1-2-value").style.color = color;
+        } else if (option == "comparison-icon") {
+          document.getElementById("kpi-1-2-title").style.color = color;
+        } else if (option == "comparison") {
+          document.getElementById("kpi-1-2-value").style.color = color;
+          document.getElementById("kpi-1-2-title").style.color = color;
+        }
+      }
+
+
+      if (kpi1_comparison_value_1>0) {
+        colorizeKPImain(config.main_element_positive_color, config.kpi_1_comparison_color_mode)
+        if (config.kpi_comparison_icon_positive != null) {
+          document.getElementById("kpi-1-icon").setAttribute("name", config.kpi_comparison_icon_positive);
+        }
+      } else if (kpi1_comparison_value_1<0) {
+        colorizeKPImain(config.main_element_negative_color, config.kpi_1_comparison_color_mode)
+        if (config.kpi_comparison_icon_negative != null) {
+          document.getElementById("kpi-1-icon").setAttribute("name", config.kpi_comparison_icon_negative);
+        }
+      }
+
+      if (kpi1_comparison_value_2>0) {
+        colorizeKPIsecondary(config.comparison_element_positive_color, config.kpi_1_comparison_color_mode, 1)
+      } else if (kpi1_comparison_value_2<0) {
+        colorizeKPIsecondary(config.comparison_element_negative_color, config.kpi_1_comparison_color_mode, 1)
       }
       
-      if (kpi1_value > 0) {
-        var kpi1_value_and_icon = "<box-icon type='solid' name='" + kpi1_positive_icon + "'></box-icon></i><span>" + kpi1_value + "</span>";
-      } else {
-        var kpi1_value_and_icon = "<box-icon type='solid' name='" + kpi1_negative_icon + "'></box-icon></i><span>" + kpi1_value + "</span>";
-      }
-      
-
-      addKPI(kpi1_value_and_icon, kpi1_title, 'kpi-1');
 
       done()
     }
