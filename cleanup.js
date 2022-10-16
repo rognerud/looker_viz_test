1014ac5d8ece145a6e30aa34699b71cf74fa55a5
girog161 <gisle.rognerud@tv2.no>
2022-10-17 00:16:22 +0200

    lot of stuff working

---
+++ cleanup.js
@@ -0,0 +1,28 @@
+    <div class="content-wrapper">
+      <div class="card" id="kpi-1">
+        <div class="card-icon">
+          <box-icon id="kpi-1-icon" type="solid" size="15vh" color="#eb4034" name="rocket"></box-icon>
+        </div>
+        <div class="card-value" id="kpi-1-value">50%</div>
+        <div class="card-title" id="kpi-1-title">title</div>
+        <div class="card-comparison" id="kpi-1-comparisons">
+          <span class="card-comparison-sign" id="kpi-1-1-sign"></span><span class="card-comparison-value" id="kpi-1-1-value">15 pp</span>
+          <span class="card-comparison-title" id="kpi-1-1-title">mot y-1</span>
+            <span class="card-comparison-sign" id="kpi-1-2-sign"></span><span class="card-comparison-value" id="kpi-1-2-value">10 pp</span>
+          <span class="card-comparison-title" id="kpi-1-2-title">mot m-1</span>   
+        </div>
+      </div>
+      <div class="card" id="kpi-2">
+        <div class="card-icon">
+          <box-icon id="kpi-2-icon" type="solid" size="15vh" color="#eb4034" name=""></box-icon>
+        </div>
+        <div class="card-value" id="kpi-2-value"></div>
+        <div class="card-title" id="kpi-2-title"></div>
+        <div class="card-comparison" id="kpi-2-comparisons">
+          <span class="card-comparison-sign" id="kpi-2-1-sign"></span><span class="card-comparison-value" id="kpi-2-1-value"></span>
+          <span class="card-comparison-title" id="kpi-2-1-title"></span>
+            <span class="card-comparison-sign" id="kpi-2-2-sign"></span><span class="card-comparison-value" id="kpi-2-2-value"></span>
+          <span class="card-comparison-title" id="kpi-2-2-title"></span>   
+        </div>
+      </div>
+    </div> 
